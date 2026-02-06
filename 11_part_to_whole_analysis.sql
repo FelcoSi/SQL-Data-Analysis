ebe9ee9edb5bd4862a3c0530418a29dcb2e50e37
FelcoSi <felixsiame1@outlook.com>
2026-02-06 13:44:44 -0500

    Update 11_part_to_whole_analysis.sql

--- 11_part_to_whole_analysis.sql
+++ 11_part_to_whole_analysis.sql
@@ -17,8 +17,8 @@ WITH category_sales AS (
     SELECT
         p.category,
         SUM(f.sales_amount) AS total_sales
-    FROM gold.fact_sales f
-    LEFT JOIN gold.dim_products p
+    FROM fact_sales f
+    LEFT JOIN dim_products p
         ON p.product_key = f.product_key
     GROUP BY p.category
 )
@@ -28,4 +28,5 @@ SELECT
     SUM(total_sales) OVER () AS overall_sales,
     ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
 FROM category_sales
+
 ORDER BY total_sales DESC;
